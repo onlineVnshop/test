23e8259e704ed0a64aceed19433e39fb5e45c060
zhanghl <zhanghl@quickbusiness.cn>
2018-03-15 19:41:59 +0800

    第二天

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-	// 右侧组件栏跟随事件
+(function(){
+		// 右侧组件栏跟随事件
 		var shop=document.querySelector(".shop");
 		console.log(shop)
 		shop.onclick=function(){
@@ -7,6 +8,13 @@
 			console.log(model)
 		model.style.display="block";
 		}
+		// 模拟单选按钮
+		// function radio(){
+		// 	var bigCircle=document.querySelector(".big_circle");
+		// 	var littleCircle=document.querySelector(".little_circle");
+		// 	if()
+		// }
+})()
 	
 		
 	
