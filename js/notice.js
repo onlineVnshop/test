84105bef736070195903974407dcd77ba57682a0
DreamLi <535789859@qq.com>
2018-03-26 23:36:59 +0800

    公告完成

--- js/notice.js
+++ js/notice.js
@@ -1,14 +1,31 @@
 var _this;
-
+$(".box").clone().prependTo(".notice_compontent");
 $(".notice_compontent").on("click",".box",function(){
 	_this=$(this);
+	$(".editor_menu_item").hide();
+	_this.find(".editor_menu_item").show();
+	
+	
+	
 	$(".input_notice").on("keyup","input",function(){
-		console.log(1)
+		console.log($(".notice_nav_content").width())
 		_this.find(".notice_nav_content").html($(this).val())
+		var font=_this.find(".notice_nav_content").css("font-size").slice(0,2);
+		var len=_this.find(".notice_nav_content").html().length;
+		var str=_this.find(".notice_nav_content").html();
+		console.log(typeof font);
+		console.log(len);
+		if(parseInt(font)*len>_this.find(".notice_nav_content_wrap").width()){			
+			_this.find(".notice_nav_content").html('<marquee direction="left" scrollamount="2">'+str+'</marquee>')
+		}else{
+			_this.find(".notice_nav_content").html(str)
+			_this.find(".notice_nav_content").html(str)
+			
+		}
 	})
 	$(document).mousemove(function(){
-		console.log(1)
-		$(".notice_container").css("background","#"+$(".color").val())		
+		// console.log(1)
+		_this.find(".notice_container").css("background","#"+_this.find(".bg").val())		
 			
 		$(".bg_color_reset").click(function(e){
 			$(".bg").val("FFF8E9")
@@ -18,7 +35,7 @@ $(".notice_compontent").on("click",".box",function(){
 		})
 	})
 	$(document).mousemove(function(){
-		$(".notice_nav_content").css("color","#"+$(".font").val())		
+		_this.find(".notice_nav_content").css("color","#"+_this.find(".font").val())		
 		$(".font_color_reset").click(function(e){
 			$(".font").val("666666")
 			$(".font").css("background-color","#666666");
@@ -27,4 +44,7 @@ $(".notice_compontent").on("click",".box",function(){
 		})
 
 	})
+	
 })
+
+
